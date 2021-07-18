4be1ebd289bbd4fcedf669a0c7320e4de68f3bbf
odrc <odrodriguezc@gmail.com>
2021-07-18 16:48:04 +0200

    Add validations

--- app/models/requestor.rb
+++ app/models/requestor.rb
@@ -1,6 +1,14 @@
 class Requestor < ApplicationRecord
+  # Validations
+  validates :name, presence: true, length: { maximum: 250}
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
+  validates :phone_number, numericality: true, allow_blank: true
+  validates :biography, length: { maximum: 1000}, allow_blank: true
+
+  # 1:n relation with space request
   has_many :space_requests
 
+  # Callback to create space request
   after_create :create_space_request
 
   def create_space_request
--- app/models/space_request.rb
+++ app/models/space_request.rb
@@ -1,7 +1,11 @@
 class SpaceRequest < ApplicationRecord
+  # Create a 'magic' method by key to set the status value
   enum status: [:unconfirmed, :confirmed, :accepted, :expired]
+
+  # n:1 Relation with requestor
   belongs_to :requestor
 
+  # Method to modify status (call enum magi method) with another name (accept instead of accepted)
   def accept!
     accepted!
   end
