1270cf571e58ef0af1147288d45659a43f430168
odrc <odrodriguezc@gmail.com>
2021-07-21 22:17:23 +0200

    Create fake data to seed db and test application with control cases

--- app/models/requestor.rb
+++ app/models/requestor.rb
@@ -2,13 +2,13 @@ class Requestor < ApplicationRecord
   # Validations
   validates :name, presence: true, length: { maximum: 250}
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
-  validates :phone_number, numericality: true, allow_blank: true
+  validates :phone_number, length: {minimum: 7, maximum: 20}, allow_blank: true
   validates :biography, length: { maximum: 1000}, allow_blank: true
 
   # 1:n relation with space request
-  has_many :space_requests
+  has_many :space_requests, dependent: :destroy
   # 1:n relation with contracts
-  has_many :contracts
+  has_many :contracts, dependent: :destroy
 
   # Callback to create space request automatically
   after_create :create_space_request
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# clean DB
+Requestor.destroy_all
+SpaceRequest.destroy_all
+Contract.destroy_all
+
+Faker::Config.locale = 'fr'
+
+# Generate fake data
+10.times do |index|
+  Requestor.create!(
+    name: Faker::Name.unique.name,
+    email: "odrodriguezc+#{index}@gmail.com",
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    biography: Faker::Lorem.paragraph
+  )
+end
+
+first_space_request_id = SpaceRequest.first.id
+2.times do |index|
+  SpaceRequest.find(first_space_request_id + index + 1).accept!
+end
+
+first_contract_id = Contract.first.id
+1.times do |index|
+  Contract.find(first_contract_id + index + 1).update(countdown_start_at: Faker::Date.between(from: 40.days.ago, to: 32.days.ago))
+end
+
+3.times do |index|
+  SpaceRequest.unconfirmed[index].update(countdown_start_at: Faker::Date.between(from: 100.days.ago, to: 91.days.ago))
+end
+p "Created 10 requestors."
