14784008f7b421938b2c70a2c1cf0b4a15e3a5c2
odrc <odrodriguezc@gmail.com>
2021-07-22 19:26:20 +0200

    fix bug config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,12 +122,12 @@ Rails.application.configure do
   #Action mailer
   config.action_mailer.default_url_options = { host: 'https://workaround-odrc.herokuapp.com/' } # for absolute urls in email
   config.action_mailer.asset_host = 'https://workaround-odrc.herokuapp.com/' # for image URLs in HTML email
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
