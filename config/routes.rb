ca1d478190674a44d62f3ba0b6ea4f5747974664
odrc <odrodriguezc@gmail.com>
2021-07-22 17:04:28 +0200

    fix routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root '/home', to: 'home#index'
+  root to: 'home#index'
 
   resources :requestors, only: [:new, :create]
   get 'space_requests/:id/confirm', to: 'space_requests#confirm', as: 'confirm_space_request'
